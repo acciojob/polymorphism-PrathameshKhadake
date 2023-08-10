e3d5fbbba9cbd99ccd0d271b5902884daf38d4cf
Prathamesh Khadake <pratham14785@gmail.com>
2023-08-10 21:08:02 +0530

    Attempt-5

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,10 +2,16 @@ package com.driver;
 
 public class Main {
 
-    Product p = new Product();
-    int prd1 = p.product(1,2);
-    int prd2 = p.product(1, 2, 3);
-    double prd3 = p.product(2.0, 2.0);
+    public static void main(String[] args){
 
+        Product p = new Product();
+
+        int prd1 = p.product(1, 2);
+
+        int prd2 = p.product(1, 2, 3);
+
+        double prd3 = p.product(2.0, 2.0);
+
+    }
 
 }
