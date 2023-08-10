b24569203d074e64a4c13e0b5da8b6579c8274d5
Prathamesh Khadake <pratham14785@gmail.com>
2023-08-10 15:32:51 +0530

    Attempt-2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,10 +5,13 @@ import java.sql.SQLOutput;
 public class Main {
 
     Product p = new Product(); // Task-2: Object of product called P
+    p.product();
+    p.product();
+    p.product();
 
-    int ans1 = p.product(1, 2); // Task-3: call this method using Product class p
-    int ans2  = p.product(1, 2, 3); // Task-4: call this method using Product class p
-    double ans3 = p.product(2.0, 2.0); // Task-5: call this method using Product class p
+    //int ans1 = p.product(); // Task-3: call this method using Product class p
+    //int ans2  = p.product(); // Task-4: call this method using Product class p
+    //double ans3 = p.product(); // Task-5: call this method using Product class p
 
 //    System.out.println(ans1);
 //    System.out.println(ans2);
@@ -19,15 +22,15 @@ public class Main {
 
     static class Product{ // Task-1: class of Product
         public static int product(int x, int y){ // Task-3: create a method of product(int x, int y)
-            return x * y;
+
         }
 
         public static int product(int x, int y, int z){ //Task-4: create a method of product(int x, int y, int z)
-            return x * y * z;
+
         }
 
         public static double product(double x, double y){ //Task-5: create a method of product(double x, double y)
-            return x * y;
+
         }
 
     }
